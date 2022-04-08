ae1194794a7d427cf6f5588a365c1949e96ac338
John <johnathanlee72@hotmail.com>
2022-04-07 22:17:18 -0700

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Johnathan Lee on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
